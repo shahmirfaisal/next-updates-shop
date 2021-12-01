325a656c0c933c042abcb92fd98aecc41ea3d7b5
Shahmir Faisal <shahmir049@gmail.com>
2021-12-01 16:03:26 +0500

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import "react-notifications/lib/notifications.css";
 import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
 import { NotificationContainer } from "react-notifications";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -14,6 +17,18 @@ export default function App({ Component, pageProps }) {
     }
   }, []);
 
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   const theme = createTheme({
     typography: {
       fontFamily: '"Montserrat", sans-serif',
@@ -22,6 +37,26 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
